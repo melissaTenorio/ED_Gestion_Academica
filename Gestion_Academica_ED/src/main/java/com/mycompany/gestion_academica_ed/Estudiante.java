07c4cf294ee4c40c283460c773dc0e4531b55e55
melissaTenorio <melissa.tenorio247427@potros.itson.edu.mx>
2026-05-15 01:58:58 -0700

    Clase estudante y arregloDinamico

--- Gestion_Academica_ED/src/main/java/com/mycompany/gestion_academica_ed/Estudiante.java
+++ Gestion_Academica_ED/src/main/java/com/mycompany/gestion_academica_ed/Estudiante.java
@@ -14,6 +14,73 @@ public class Estudiante {
     private String telefono;
     private String correo;
     private String direccion;
-    //falta atributo de arreglo de calificaciones del tipo ARREGLO DINAMICO
-    
+private Arreglo_Dinamico calificaciones;   
+
+/**
+ * metodos para agregar calificaciones, calcular promedio y mostrar dichas calificaciones
+ */
+   public void agregarCalificacion(double cal){this.calificaciones.agregar(cal);}
+
+   public double calcularPromedio(){
+       if (calificaciones.obtenerTamaño()==0) return 0.0;
+       double suma=0.0;
+       for (int i = 0; i < calificaciones.obtenerTamaño(); i++) {
+           suma+=(double)calificaciones.obtener(i); //se suman/acumulan las calificaciones encontradas
+       }return suma/calificaciones.obtenerTamaño();
+   }
+   
+   public void mostrarCalificaciones(){}
+/**
+ * getters y setters
+ * @return 
+ */
+public String getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(String Matricula) {
+        this.Matricula = Matricula;
+    }
+
+    public String getNomCompleto() {
+        return nomCompleto;
+    }
+
+    public void setNomCompleto(String nomCompleto) {
+        this.nomCompleto = nomCompleto;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Arreglo_Dinamico getCalificaciones() {
+        return calificaciones;
+    }
+
+    public void setCalificaciones(Arreglo_Dinamico calificaciones) {
+        this.calificaciones = calificaciones;
+    }
+
+
 }
